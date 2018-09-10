d053d739e2b35bf60d266b1a19d7a200a0185257
abisummers <hello@abisummers.com>
2018-09-10 10:18:59 +0200

    changed database name

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const passportSetup = require("./config/passport/passport-setup.js");
 
 mongoose
   .connect(
-    "mongodb://localhost/project-two",
+    "mongodb://localhost/project-platform",
     { useNewUrlParser: true }
   )
   .then(x => {
