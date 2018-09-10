1cf0f6e3dd0eceadfcde9f16bd685fc1b20214a1
abisummers <hello@abisummers.com>
2018-09-10 16:34:16 +0200

    added nav bar

--- routes/ideas-router.js
+++ routes/ideas-router.js
@@ -1,58 +1,52 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 const Idea = require("../models/idea-model.js");
 
-
 router.get("/ideas", (req, res, next) => {
-    if (!req.user) {
-      req.flash("error", "You must be logged in to see this page");
-      res.redirect("/");
-      return; 
-    }
-    res.render("ideas-views/ideas-list.hbs");
+  if (!req.user) {
+    req.flash("error", "You must be logged in to see this page");
+    res.redirect("/");
+    return;
+  }
+  res.render("ideas-views/ideas-list.hbs");
 });
 
 router.get("/ideas/:ideaId", (req, res, next) => {
-    if (!req.user) {
-      req.flash("error", "You must be logged in to see this page");
-      res.redirect("/");
-      return; 
-    }
+  if (!req.user) {
+    req.flash("error", "You must be logged in to see this page");
+    res.redirect("/");
+    return;
+  }
 
-    const {ideaId} = request.params;
+  const { ideaId } = request.params;
 
-    Idea.findById({ideaId})
+  Idea.findById({ ideaId })
     .then(ideaDoc => {
-        res.locals.myIdea = ideaDoc;
-        res.render("ideas-views/idea-details.hbs");
+      res.locals.myIdea = ideaDoc;
+      res.render("ideas-views/idea-details.hbs");
     })
-    .catch(err =>next(err));
-    
-});
-
-
-router.get("/add-idea", (req, res, next) => {
-    if (!req.user) {
-      req.flash("error", "You must be logged in to see this page");
-      res.redirect("/");
-      return; 
-    }
-    res.render("ideas-views/idea-form.hbs");
-});
-
-
-router.post("/process-idea", (req, res, next) => {
-    const { name, description, deadline, pictureUrl } = req.body;
-    const user = req.user._id;
-
-    Idea.create({ name, description, deadline, pictureUrl, user })
-    .then(ideaDoc => {
-        req.flash("success", "Idea created successfully!");
-        res.redirect("/ideas");
-      })
     .catch(err => next(err));
 });
 
+router.get("/add-idea", (req, res, next) => {
+  if (!req.user) {
+    req.flash("error", "You must be logged in to see this page");
+    res.redirect("/");
+    return;
+  }
+  res.render("ideas-views/idea-form.hbs");
+});
 
+router.post("/process-idea", (req, res, next) => {
+  const { name, description, deadline, pictureUrl } = req.body;
+  const user = req.user._id;
+
+  Idea.create({ name, description, deadline, pictureUrl, user })
+    .then(ideaDoc => {
+      req.flash("success", "Idea created successfully!");
+      res.redirect("/ideas");
+    })
+    .catch(err => next(err));
+});
 
 module.exports = router;
--- routes/projects-router.js
+++ routes/projects-router.js
@@ -1,60 +1,53 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 const Project = require("../models/project-model.js");
 
-
 router.get("/projects", (req, res, next) => {
-    if (!req.user) {
-      req.flash("error", "You must be logged in to see this page");
-      res.redirect("/");
-      return; 
-    }
-    res.render("projects-views/projects-list.hbs");
+  if (!req.user) {
+    req.flash("error", "You must be logged in to see this page");
+    res.redirect("/");
+    return;
+  }
+  //project-list NOT projects list
+  res.render("projects-views/project-list.hbs");
 });
 
 router.get("/projects/:projectId", (req, res, next) => {
-    if (!req.user) {
-      req.flash("error", "You must be logged in to see this page");
-      res.redirect("/");
-      return; 
-    }
-    
-    const {projectId} = request.params;
+  if (!req.user) {
+    req.flash("error", "You must be logged in to see this page");
+    res.redirect("/");
+    return;
+  }
 
-    Project.findById({projectId})
+  const { projectId } = request.params;
+
+  Project.findById({ projectId })
     .then(projectDoc => {
-        res.locals.myProject = projectDoc;
-        res.render("project-views/project-details.hbs");
+      res.locals.myProject = projectDoc;
+      res.render("project-views/project-details.hbs");
     })
-    .catch(err =>next(err));
-});
-
-
-router.get("/add-project", (req, res, next) => {
-    if (!req.user) {
-      req.flash("error", "You must be logged in to see this page");
-      res.redirect("/");
-      return; 
-    }
-    res.render("projects-views/project-form.hbs");
-});
-
-router.post("/process-project", (req, res, next) => {
-    const { name, description, deadline, pictureUrl, linkUrl } = req.body;
-    const user = req.user._id;
-
-    Idea.create({ name, description, deadline, pictureUrl, linkUrl, user })
-    .then(ideaDoc => {
-        req.flash("success", "Project created successfully!");
-        res.redirect("/projects");
-      })
     .catch(err => next(err));
 });
 
+router.get("/add-project", (req, res, next) => {
+  if (!req.user) {
+    req.flash("error", "You must be logged in to see this page");
+    res.redirect("/");
+    return;
+  }
+  res.render("projects-views/project-form.hbs");
+});
+
+router.post("/process-project", (req, res, next) => {
+  const { name, description, deadline, pictureUrl, linkUrl } = req.body;
+  const user = req.user._id;
+
+  Idea.create({ name, description, deadline, pictureUrl, linkUrl, user })
+    .then(ideaDoc => {
+      req.flash("success", "Project created successfully!");
+      res.redirect("/projects");
+    })
+    .catch(err => next(err));
+});
 
 module.exports = router;
-
-
-
-
-
