890534db253244fde93cc5c2ff1950c8a070192b
abisummers <hello@abisummers.com>
2018-09-11 09:05:42 +0200

    added flash messages

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -16,6 +16,7 @@ router.post("/process-signup", (req, res, next) => {
 
   User.create({ fullName, email, course, encryptedPassword, startDate })
     .then(userDoc => {
+      req.flash("success", "account created successfully");
       res.redirect("/");
     })
     .catch(err => next(err));
@@ -36,15 +37,18 @@ router.post("/process-login", (req, res, next) => {
   User.findOne({ email: { $eq: email } })
     .then(userDoc => {
       if (!userDoc) {
+        req.flash("error", "You must be logged in to see this page");
         res.redirect("/index.hbs");
         return;
       }
       const { encryptedPassword } = userDoc;
       if (!bcrypt.compareSync(userPassword, encryptedPassword)) {
+        req.flash("error", "You must be logged in to see this page");
         res.redirect("/index.hbs");
         return;
       }
       req.logIn(userDoc, () => {
+        req.flash("success", "you are logged in");
         res.redirect("/home");
       });
     })
@@ -56,7 +60,7 @@ router.post("/process-login", (req, res, next) => {
 router.get("/logout", (req, res, next) => {
   //req.logOut() is a passport method
   req.logOut();
-
+  req.flash("success", "logout successful");
   res.redirect("/");
 });
 
