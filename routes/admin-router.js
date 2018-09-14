ced98689413780b41ae56fd575da453f14f9c674
abisummers <hello@abisummers.com>
2018-09-14 10:43:39 +0200

    admin verified user page styled

--- routes/admin-router.js
+++ routes/admin-router.js
@@ -4,58 +4,52 @@ const User = require("../models/user-model.js");
 
 const router = express.Router();
 
-
-
 //------------------ADMIN HOMEPAGE-------------------------------------------
 
 router.get("/admin/homepage", (req, res, next) => {
-  if(!req.user || req.user.role !== "admin"){
+  if (!req.user || req.user.role !== "admin") {
     req.flash("error", "Only admins can see this page");
     res.redirect("/");
     return;
   }
 
-  res.render("admin-views/admin-homepage.hbs");  
+  res.render("admin-views/admin-homepage.hbs");
 });
-
-
 
 //------------------VERIFIED USERS LIST---------------------------------------
 router.get("/admin/verified-users", (req, res, next) => {
-  if(!req.user || req.user.role !== "admin"){
+  if (!req.user || req.user.role !== "admin") {
     req.flash("error", "Only admins can see this page");
     res.redirect("/");
     return;
   }
   User.find({ verified: { $eq: true } })
-    .sort({ role: 1, createdAt: 1})
+    .sort({ role: 1, createdAt: 1 })
     .then(userResults => {
       res.locals.userArray = userResults;
       res.render("admin-views/verified-users-list.hbs");
     })
-    .catch (err => next(err));
+    .catch(err => next(err));
 });
-
 
 //-----------------NON VERIFIED USERS LIST-------------------------------------
 router.get("/admin/non-verified-users", (req, res, next) => {
-  if(!req.user || req.user.role !== "admin"){
+  if (!req.user || req.user.role !== "admin") {
     req.flash("error", "Only admins can see this page");
     res.redirect("/");
     return;
   }
   User.find({ verified: { $eq: false } })
-    .sort({ createdAt: 1})
+    .sort({ createdAt: 1 })
     .then(userResults => {
       res.locals.userArray = userResults;
       res.render("admin-views/non-verified-users-list.hbs");
     })
-    .catch (err => next(err));
+    .catch(err => next(err));
 });
 
-
 //-----------------DELETE A USER-----------------------------------------
-router.get("/admin/:profileId/delete", (req, res,next)=> {
+router.get("/admin/:profileId/delete", (req, res, next) => {
   if (!req.user) {
     req.flash("error", "You must be logged in to see this page");
     res.redirect("/");
@@ -67,22 +61,19 @@ router.get("/admin/:profileId/delete", (req, res,next)=> {
     res.redirect("/home");
     return;
   }
-  
+
   const { profileId } = req.params;
 
   User.findByIdAndRemove(profileId)
-  .then(profileDoc => {
-    res.redirect("/admin/verified-users");
-  })
-  .catch(err => next(err));
+    .then(profileDoc => {
+      res.redirect("/admin/verified-users");
+    })
+    .catch(err => next(err));
 });
-
-
-
 
 //-----------------APPROVE A USER----------------------------------------
 
-router.get("/admin/:profileId/approve", (req, res,next)=> {
+router.get("/admin/:profileId/approve", (req, res, next) => {
   if (!req.user) {
     req.flash("error", "You must be logged in to see this page");
     res.redirect("/");
@@ -98,17 +89,16 @@ router.get("/admin/:profileId/approve", (req, res,next)=> {
   const { profileId } = req.params;
 
   User.findByIdAndUpdate(
-    profileId, 
-    { $set: { verified: true }},
-    { runValidators: true },
+    profileId,
+    { $set: { verified: true } },
+    { runValidators: true }
   )
-  .then(userDoc => {
-    //save a flash message to display in the HOME page
-    req.flash("success", "User approved");
-    res.redirect("/admin/non-verified-users");
-  })
-  .catch( err => next(err));
+    .then(userDoc => {
+      //save a flash message to display in the HOME page
+      req.flash("success", "User approved");
+      res.redirect("/admin/non-verified-users");
+    })
+    .catch(err => next(err));
 });
-
 
 module.exports = router;
